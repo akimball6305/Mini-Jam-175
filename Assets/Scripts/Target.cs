c5ade42896f19527965cc7f6037ac609df87a1ed
bardyswans <swansonbrady0@gmail.com>
2025-01-03 17:10:07 -0600

    created a demon prefab
    demon prefab has the targetr script and the blood effect when it is killed

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    [SerializeField] ParticleSystem BloodSplash;
+    [SerializeField] ParticleSystem BloodSplash;  // Assign the prefab in Inspector
 
     public float health = 10f;
 
@@ -19,10 +19,25 @@ public class Target : MonoBehaviour
     {
         if (BloodSplash != null)
         {
-            ParticleSystem splash = Instantiate(BloodSplash, transform.position, Quaternion.identity);      //plays bloodsplash when target dies
+            // Instantiate at object position with slight upward offset
+            Vector3 spawnOffset = Vector3.up * 1f;
+            ParticleSystem splash = Instantiate(BloodSplash, transform.position + spawnOffset, Quaternion.identity);
+
+            // Force the particle system to emit immediately
+            splash.transform.localScale = Vector3.one * 2;  // Scale up effect if necessary
             splash.Play();
-            Destroy(splash.gameObject, splash.main.duration); // Destroy after particle finishes
+            splash.Emit(100);  // Forces instant emission of 10 particles
+            Debug.Log("Blood effect instantiated and emitting.");
+
+            // Destroy the particle system after its duration
+            Destroy(splash.gameObject, splash.main.duration);
         }
+        else
+        {
+            Debug.LogWarning("BloodSplash particle system not assigned!");
+        }
+
+        // Destroy the target object
         Destroy(gameObject);
     }
 }

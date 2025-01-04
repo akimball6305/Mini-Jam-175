6ce5302882d094178ba00f8fbf762da432fba6ff
bardyswans <swansonbrady0@gmail.com>
2025-01-04 15:01:29 -0600

    powerup started
    started work on a freeze enemy script

---
+++ Assets/Scripts/FreezeEnemies.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class FreezeEnemies : MonoBehaviour
+{
+    public EnemyFollow script;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

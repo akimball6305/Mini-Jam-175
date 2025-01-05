340e709d3272fa2f3c3d1fb0e13145d5b43f7da5
akimball6305 <aiden.kimball6305@gmail.com>
2025-01-05 17:54:36 -0600

    Addded audio and end scene for death fixed death not working.

--- Assets/Scripts/KillJohnLennon.cs
+++ Assets/Scripts/KillJohnLennon.cs
@@ -2,18 +2,18 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class KillJohnLennon : MonoBehaviour
 {
-    public int Respawn;
+    public int Respawn = 2;
    // [SerializeField] GameObject deathUI;
     //[SerializeField] GameObject mainUI;
 
-    ScoreKeeper scoreKeeper;
+    //ScoreKeeper scoreKeeper;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             //deathUI.SetActive(true);
-            scoreKeeper.StopTimer();
+            //scoreKeeper.StopTimer();
            // mainUI.SetActive(false);
             SceneManager.LoadScene(Respawn);
         }

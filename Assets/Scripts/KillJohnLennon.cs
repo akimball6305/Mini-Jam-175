df8faa58f9c8046ef324e3a529a422d30d1aaee7
akimball6305 <aiden.kimball6305@gmail.com>
2025-01-05 16:11:26 -0600

    Made Canvas have death screen
    made death screen when play gets hit.

--- Assets/Scripts/KillJohnLennon.cs
+++ Assets/Scripts/KillJohnLennon.cs
@@ -3,12 +3,18 @@ using UnityEngine.SceneManagement;
 public class KillJohnLennon : MonoBehaviour
 {
     public int Respawn;
+    [SerializeField] GameObject deathUI;
+    [SerializeField] GameObject mainUI;
+
+    ScoreKeeper scoreKeeper;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene(Respawn);
+            deathUI.SetActive(true);
+            scoreKeeper.StopTimer();
+            mainUI.SetActive(false);
         }
     }
 
